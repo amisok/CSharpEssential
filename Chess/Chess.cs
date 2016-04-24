42413c31062b3a5db999a5b40445e2651f62f3ab
Ann Marchenko <marchenko.ann@gmai.com>
2016-04-25 00:11:36 +0300

    Add project Chess

---
+++ Chess/Chess.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Chess
+    {
+        public Chess (int colour, char name)
+        {
+            this.colour = colour;  //0-black, 1-white
+            this.name = name;
+        }
+
+        protected int colour;
+        protected char name;
+
+        public char GetName()
+        {
+            return name;
+        }
+
+        //public virtual void Move()
+        //{
+
+        //}
+    }
+
+    class Pawn : Chess
+    {
+        public Pawn(int colour):base(colour, ((colour == 0)?'p':'P')) 
+        {
+        }
+        
+    }
+
+    class Rook : Chess
+    {
+        public Rook(int colour) : base(colour, ((colour == 0) ? 'r' : 'R'))
+        {
+        }
+
+    }
+
+    class Knight : Chess
+    {
+        public Knight(int colour) : base(colour, ((colour == 0) ? 'n' : 'N'))
+        {
+        }
+
+    }
+
+    class Bishop : Chess
+    {
+        public Bishop(int colour) : base(colour, ((colour == 0) ? 'b' : 'B'))
+        {
+        }
+
+    }
+
+    class Queen : Chess
+    {
+        public Queen(int colour) : base(colour, ((colour == 0) ? 'q' : 'Q'))
+        {
+        }
+
+    }
+
+    class King : Chess
+    {
+        public King(int colour) : base(colour, ((colour == 0) ? 'k' : 'K'))
+        {
+        }
+
+    }
+
+
+
+}
---
+++ Chess/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Desk desk = new Desk();
+            desk.Show();
+            Console.ReadKey();
+        }
+    }
+}
