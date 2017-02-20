5637bd33a59d6d75b5a261aaffc31f2c4f901573
Ann Marchenko <marchenko.ann@gmail.com>
2017-02-21 00:47:43 +0200

    Add project Serializer

---
+++ Serializer/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Serializer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxRawData = new System.Windows.Forms.ListBox();
+            this.listBoxJsonData = new System.Windows.Forms.ListBox();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(13, 13);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(75, 23);
+            this.buttonRandom.TabIndex = 0;
+            this.buttonRandom.Text = "Random";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(12, 42);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(13, 71);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "file name";
+            // 
+            // listBoxRawData
+            // 
+            this.listBoxRawData.FormattingEnabled = true;
+            this.listBoxRawData.Location = new System.Drawing.Point(119, 13);
+            this.listBoxRawData.Name = "listBoxRawData";
+            this.listBoxRawData.Size = new System.Drawing.Size(353, 394);
+            this.listBoxRawData.TabIndex = 4;
+            // 
+            // listBoxJsonData
+            // 
+            this.listBoxJsonData.FormattingEnabled = true;
+            this.listBoxJsonData.Location = new System.Drawing.Point(478, 13);
+            this.listBoxJsonData.Name = "listBoxJsonData";
+            this.listBoxJsonData.Size = new System.Drawing.Size(377, 394);
+            this.listBoxJsonData.TabIndex = 5;
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(13, 129);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFileName.TabIndex = 6;
+            this.textBoxFileName.Text = "file name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 411);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.listBoxJsonData);
+            this.Controls.Add(this.listBoxRawData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonRandom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxRawData;
+        private System.Windows.Forms.ListBox listBoxJsonData;
+        private System.Windows.Forms.TextBox textBoxFileName;
+    }
+}
+
---
+++ Serializer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Serializer
+{
+    public partial class Form1 : Form
+    {
+        int[][] array;
+
+        public Form1()
+        {
+            array = new int[20][];
+            for (int i = 0; i < 20; i++)
+            {
+                array[i] = new int[15];
+            }
+            InitializeComponent();
+        }
+
+        private void RandomFill(int[][] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = rand.Next(0, 15);
+                }
+            }
+        }
+        private void PrintRawData(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                string tmp = "";
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    tmp = tmp + array[i][j].ToString() + " ";
+                }
+                listBoxRawData.Items.Add(tmp);
+            }
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            RandomFill(array);
+            PrintRawData(array);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+            FileStream stream = new FileStream(textBoxFileName.Text, FileMode.Create);
+            DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(int[][]));
+            json.WriteObject(stream, array);
+            stream.Close();
+
+            StreamReader reader = new StreamReader(textBoxFileName.Text);
+            listBoxJsonData.Items.Add(reader.ReadToEnd());
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
