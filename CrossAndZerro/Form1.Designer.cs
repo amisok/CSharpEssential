0acf209ba84712db9f2643faf221508766a0fceb
Ann Marchenko <marchenko.ann@gmail.com>
2017-02-21 00:33:49 +0200

    Add project CrossAndZerro

---
+++ CrossAndZerro/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CrossAndZerro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.button00 = new System.Windows.Forms.Button();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.labelPlayer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(13, 13);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 0;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(12, 136);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 13);
+            this.labelMessage.TabIndex = 3;
+            // 
+            // button00
+            // 
+            this.button00.Location = new System.Drawing.Point(13, 43);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(30, 30);
+            this.button00.TabIndex = 4;
+            this.button00.Text = "0";
+            this.button00.UseVisualStyleBackColor = true;
+            this.button00.Click += new System.EventHandler(this.button00_Click);
+            // 
+            // button01
+            // 
+            this.button01.Location = new System.Drawing.Point(43, 43);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(30, 30);
+            this.button01.TabIndex = 5;
+            this.button01.Text = "X";
+            this.button01.UseVisualStyleBackColor = true;
+            this.button01.Click += new System.EventHandler(this.button01_Click);
+            // 
+            // button02
+            // 
+            this.button02.Location = new System.Drawing.Point(73, 43);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(30, 30);
+            this.button02.TabIndex = 6;
+            this.button02.Text = "0";
+            this.button02.UseVisualStyleBackColor = true;
+            this.button02.Click += new System.EventHandler(this.button02_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(73, 73);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(30, 30);
+            this.button12.TabIndex = 9;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(43, 73);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(30, 30);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "X";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(13, 73);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(30, 30);
+            this.button10.TabIndex = 7;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(73, 103);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(30, 30);
+            this.button22.TabIndex = 12;
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(43, 103);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(30, 30);
+            this.button21.TabIndex = 11;
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(13, 103);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(30, 30);
+            this.button20.TabIndex = 10;
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // labelPlayer
+            // 
+            this.labelPlayer.AutoSize = true;
+            this.labelPlayer.Location = new System.Drawing.Point(10, 167);
+            this.labelPlayer.Name = "labelPlayer";
+            this.labelPlayer.Size = new System.Drawing.Size(0, 13);
+            this.labelPlayer.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(116, 156);
+            this.Controls.Add(this.labelPlayer);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button02);
+            this.Controls.Add(this.button01);
+            this.Controls.Add(this.button00);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.buttonReset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Label labelPlayer;
+    }
+}
+
---
+++ CrossAndZerro/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrossAndZerro
+{
+    public partial class Form1 : Form
+    {
+        Game game;
+        public Form1()
+        {
+            InitializeComponent();
+            game = new Game();
+            game.changeState += FormChangeStateHandler;
+            ShowGame();
+            labelMessage.Text = "New game";
+        }
+
+        private void FormChangeStateHandler(CheckResult p)
+        {
+            if (p == CheckResult.CrossWin)
+            {
+                LockButtons();
+                labelMessage.Text = "The game is finished. Cross is winner";
+            }
+            else if (p == CheckResult.ZeroWin)
+            {
+                LockButtons();
+                labelMessage.Text = "The game is finished. Zerro is winner";
+            }
+            else if (p == CheckResult.DeadHeat)
+            {
+                LockButtons();
+                labelMessage.Text = "The game is finished. It's deap heap";
+            }
+        }
+
+        private void LockButtons()
+        {
+            button00.Enabled = false;
+            button01.Enabled = false;
+            button02.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button20.Enabled = false;
+            button21.Enabled = false;
+            button22.Enabled = false;
+        }
+        private void UnLockButtons()
+        {
+            button00.Enabled = true;
+            button01.Enabled = true;
+            button02.Enabled = true;
+            button10.Enabled = true;
+            button11.Enabled = true;
+            button12.Enabled = true;
+            button20.Enabled = true;
+            button21.Enabled = true;
+            button22.Enabled = true;
+        }
+
+        public void ShowGame()
+        {
+            button00.Text = PlayerToString(game.map[0, 0]);
+            button01.Text = PlayerToString(game.map[0, 1]);
+            button02.Text = PlayerToString(game.map[0, 2]);
+            button10.Text = PlayerToString(game.map[1, 0]);
+            button11.Text = PlayerToString(game.map[1, 1]);
+            button12.Text = PlayerToString(game.map[1, 2]);
+            button20.Text = PlayerToString(game.map[2, 0]);
+            button21.Text = PlayerToString(game.map[2, 1]);
+            button22.Text = PlayerToString(game.map[2, 2]);
+            ShowPlayer();
+
+        }
+        private void ShowPlayer()
+        {
+            if (game.currPlayer == Player.Cross)
+            {
+                labelPlayer.Text = "Next move: Cross";
+            }
+            else if (game.currPlayer == Player.Zero)
+            {
+                labelPlayer.Text = "Next move: Zero";
+            }
+        }
+        private string PlayerToString(Player player)
+        {
+            string str = "";
+            if (player == Player.Cross)
+            {
+                str = "X";
+            }
+            else if (player == Player.Zero)
+            {
+                str = "0";
+            }
+            return str;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            game = new Game();
+            UnLockButtons();
+            game.changeState += FormChangeStateHandler;
+            ShowGame();
+            labelMessage.Text = "New game";
+        }
+
+
+
+        private void button00_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                labelMessage.Text = "";
+                game.SetPosition(0, 0);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button01_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(0, 1);
+                ShowGame();
+
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button02_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(0, 2);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(1, 0);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(1, 1);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(1, 2);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(2, 0);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(2, 1);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelMessage.Text = "";
+                game.SetPosition(2, 2);
+                ShowGame();
+            }
+            catch (Exception ex)
+            {
+                labelMessage.Text = ex.Message;
+            }
+        }
+    }
+
+    class Game
+    {
+        public delegate void ChangeState(CheckResult newState);
+        public event ChangeState changeState;
+        public Player[,] map;
+        public Player currPlayer;
+
+        CheckResult currState = CheckResult.Continue;
+
+        private void OnChangeState()
+        {
+            CheckResult newState = Check();
+            if (currState != newState)
+            {
+                currState = newState;
+                if (changeState != null)
+                    changeState(currState);
+            }
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            if (map[x, y] != 0)
+            {
+                throw new Exception("Action is not allowed");
+            }
+            if (x > map.GetLength(0) || x < 0 || y > map.GetLength(1) || y < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            map[x, y] = currPlayer;
+
+            ChangePlayer();
+            OnChangeState();
+        }
+
+        public CheckResult Check()
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    if (map[i, j] == map[i, j + 1] && map[i, j] == map[i, j + 2] && map[i, j] != Player.Empty)
+                    {
+                        if (map[i, j] == Player.Cross)
+                            return CheckResult.CrossWin;
+                        else
+                            return CheckResult.ZeroWin;
+                    }
+                }
+            }
+
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    if (map[i, j] == map[i + 1, j] && map[i, j] == map[i + 2, j] && map[i, j] != Player.Empty)
+                    {
+                        if (map[i, j] == Player.Cross)
+                            return CheckResult.CrossWin;
+                        else
+                            return CheckResult.ZeroWin;
+                    }
+                }
+            }
+
+            if (map[0, 0] == map[1, 1] && map[0, 0] == map[2, 2] && map[0, 0] != Player.Empty)
+            {
+                if (map[0, 0] == Player.Cross)
+                    return CheckResult.CrossWin;
+                else
+                    return CheckResult.ZeroWin;
+            }
+
+            if (map[0, 2] == map[1, 1] && map[0, 2] == map[2, 0] && map[0, 2] != Player.Empty)
+            {
+                if (map[0, 2] == Player.Cross)
+                    return CheckResult.CrossWin;
+                else
+                    return CheckResult.ZeroWin;
+            }
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == Player.Empty)
+                        return CheckResult.Continue;
+                }
+            }
+            return CheckResult.DeadHeat;
+        }
+
+        public void ChangePlayer()
+        {
+            currPlayer = (currPlayer == Player.Cross) ? Player.Zero : Player.Cross;
+        }
+
+        public Game(Player player)
+        {
+            map = new Player[3, 3];
+            currPlayer = player;
+        }
+        public Game()
+        {
+            map = new Player[3, 3];
+            currPlayer = Player.Cross;
+        }
+    }
+
+    enum Player
+    {
+        Empty = 0,
+        Cross = 1,
+        Zero = 2
+    }
+
+    enum CheckResult
+    {
+        Continue,
+        CrossWin,
+        ZeroWin,
+        DeadHeat
+    }
+}
